83ea69f09e9b7f3c9ec872cb1dfe6cfdd012d8b9
dycoon <kuroda.dycoon@gmail.com>
2017-05-09 19:10:46 +0900

    add: initial actions

--- src/Boot.js
+++ src/Boot.js
@@ -1,7 +1,9 @@
 import React from "react";
-import { createStore} from "redux";
+import { createStore } from "redux";
 import { Provider } from "react-redux";
 import Main from "./components/Main";
+
+const store = createStore(reducer);
 
 const Boot = () => (
     <Main />
--- src/reducers/todos.js
+++ src/reducers/todos.js
@@ -1,8 +1,30 @@
 
+var initialState = () => ({
+    nextTodoId: 1,
+    todos: [],
+});
 
-const todos = (state = [], action) => {
+const createTodo= (nextId, text) => ({
+    {
+        id: nextId,
+        text,
+        completed: false,
+    }
+});
+
+
+const todos = (state = initialState(), action) => {
 
     switch(action.type) {
+        case "ADD_TODO":
+            return {
+                ...state,
+                nextTodoId: state.nextTodoId + 1,
+                todos: [
+                    state.todos...,
+                    createTodo(state.nextTodoId, action.text),
+                ]
+            }
         default:
             return state;
     }
