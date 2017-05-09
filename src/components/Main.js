9502a681da11a4b37ae49affb1efed3b0c08aad5
dycoon <kuroda.dycoon@gmail.com>
2017-05-09 18:11:41 +0900

    add: main component

---
+++ src/components/Main.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const Main = () => (
+    <View style={styles.container}>
+        <Text>
+            Main
+        </Text>
+    </View>
+);
+
+export default Main;
+
+var styles = StyleSheet.create({
+    container: {
+        flex: 1
+    }
+});
